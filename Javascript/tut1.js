71f73e3af197ffaa18e3e155f84b38d82e34245c
dhanjyoti <dhanjyotir20@gmail.com>
2024-07-27 09:18:02 +0530

    first commit

---
+++ Javascript/tut1.js
@@ -0,0 +1,17 @@
+// MDN
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/What_is_JavaScript
+// JavaScript is a scripting or programming language that allows you to implement complex features on web pages — every time a web page does more than just sit there and display static information for you to look at — displaying timely content updates, interactive maps, animated 2D/3D graphics, scrolling video jukeboxes, etc.
+
+// Divide t-shirts between each boys in equal amount
+// let t_shirt = 15;
+// let boys = 3;
+// let equal_cloths = t_shirt / boys;
+// // console.log(equal_cloths)
+
+// // find the area of a rectangle
+// let a = 600;
+// let p = 20;
+// let re = (600 / 100) * 20;
+// console.log(re);
+
+
---
+++ Javascript/tut2.js
@@ -0,0 +1,51 @@
+// fin
+
+// let raju = 20;
+// let sinu = 30;
+// let nerajan = 40;
+// let sj = 50;
+
+// function add(a, b){
+//     c = a + b;
+//     return c;
+// }
+// console.log(add(raju, sinu))
+// console.log(add(nerajan, sj))
+// console.log(add(nerajan, sinu))
+
+// function sum(){
+//     a = 36;
+//     b = 54;
+//     c = 65;
+//     averageOfThree = (a+b+c)/3
+//     return averageOfThree;
+// }
+
+// before  17 June 2015 there was only "var"
+// in 2015 there was a huge modification of JS, at that time ES6 version
+// in this ES6 "let" and "const" was introduced as new variables
+
+// var nirajan = 30;    // we can redeclare a variable name
+// var nirajan = 40;
+// console.log(nirajan)
+
+// let nirajan = 30;    // we cannot redeclare a variable name
+// let nirajan = 40;
+// console.log(nirajan)
+
+// const ab = 40;   // we cannot redeclare a variable name
+// const ab = 90;
+// console.log(ab)
+
+// let a = 40;  // we can re-assign the value of the variable
+// a = 60;
+// console.log(a)
+
+// var a = 70;  // we can re-assign the value of the variable
+// a = 80;
+// console.log(a)
+
+
+// const a = 90;   // we cannot re-assign the value of a variable ones declared
+// a = 30;
+// console.log(a)

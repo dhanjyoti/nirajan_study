5c706826cf2b39f3d51d4cd7ee507bf78ede2e4b
dhanjyoti <dhanjyotir20@gmail.com>
2024-07-31 08:46:11 +0530

    day 5

---
+++ HTML/HtmlNote.md
@@ -0,0 +1,8 @@
+Block level element -> It will take the whole width.
+h1, h2 etc
+div, p
+
+Inline level element -> It will take its width only where its element contains.
+span, br
+
+create an image, in img tag, give it a class name, link to another css file -> give it a padding of 30px, give it a border of 2px, give it a margin of 20px, also give it a height of 200px & width of 300px
---
+++ Javascript/tut5.js
@@ -0,0 +1,33 @@
+// **  Exponentiation
+
+let a = 4
+// let b = a*a*a*a;
+let b = a**4;   //  it gives us the power of a number
+// console.log(b)
+
+let c = 21%2;   // It gives us the reminder from a devision
+// console.log(c)
+
+// let d = ++a;    // Increment Operator
+// let e = a++;    // It will increase
+// console.log(a)
+// console.log(e)
+
+// let f = --a;     // Decrement operator
+// let g = a--;
+// console.log(a)
+// console.log(g)
+
+// Find if a number is even;
+// Find if a number is odd;
+
+let num = 51;
+// 1 3 5 7 etc odd numbers
+// 2 4 6 8 10 etc even numbers
+
+if(num%2 === 0){
+    console.log("number is even")
+}
+else{
+    console.log("number is odd")
+}

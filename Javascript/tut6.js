075a67771f3866d009d0e831b733bd558aea8698
dhanjyoti <dhanjyotir20@gmail.com>
2024-08-01 08:57:56 +0530

    new branch created

---
+++ Javascript/tut6.js
@@ -0,0 +1,77 @@
+// Logical operators
+
+// && -> Logical AND -> both conditions must be true
+
+// if(10 > 5 && 6 <9){
+//     console.log(true);
+// } else {
+//     console.log(false)
+// }
+
+// if(20 < 10 && 4 > 2){
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// if(50 < 100 && 6 ==7)
+//     console.log(true);
+// else
+//     console.log(false);
+
+// if(true && false)
+//     console.log(true);
+// else
+//     console.log(false);
+
+// || -> Logical OR -> any one condition has to be true
+
+// if(false || true)
+//     console.log(false)
+// else
+//     console.log(true)
+
+// if(false || false)
+//     console.log(false)
+// else
+//     console.log(true)
+
+// if(false || true)
+//     console.log(false)
+// else
+//     console.log(true)
+
+// let a = "string";
+// let b = "number";
+
+// if(a > "string" || b < "number")
+//     console.log(true)
+// else
+//     console.log(false)
+
+//  ! Logical NOT -> make true statement as false & false statement as true
+
+// let a = 20;
+// if(!a){
+//     console.log("true")
+// } else {
+//     console.log("false")
+// }
+
+// let a = 20;
+// let b = 30;
+// if(a == 20 || !b)
+//     console.log(true);
+// else
+//     console.log(false);
+
+// let a = 20;
+// let b = 30;
+// if(a == 20 && !b)
+//     console.log(true);
+// else
+//     console.log(false);
+
+// John age is 24, Prem age is 26, Rohit age is 25. 
+// So create 3 function of if else condition to so that the answer is 
+// comes out to be true, with the help of Logical AND, OR, NOT respectively.
